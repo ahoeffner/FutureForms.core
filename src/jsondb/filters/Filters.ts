a4837d445b6bd7a7182cdb1716ec8b584b3cdc04
ahoeffner <alex@hoeffner.net>
2024-07-05 12:52:30 +0200

    Update Filters.ts

--- src/jsondb/filters/Filters.ts
+++ src/jsondb/filters/Filters.ts
@@ -22,11 +22,127 @@
 
 export class Filters
 {
-   public static equals(column:string, value:any) : Filter
+   public static Equals(column:string, value:any) : Filter
    {
       let filter:Filter = new Filter();
       filter["type"] = "=";
       filter["value"] = value;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static NotEquals(column:string, value:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "!=";
+      filter["value"] = value;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static Like(column:string, value:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "like";
+      filter["value"] = value;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static NotLike(column:string, value:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "like";
+      filter["value"] = value;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static GreaterThan(column:string, value:any, equals?:boolean) : Filter
+   {
+      let type:string = ">";
+      if (equals) type += "=";
+
+      let filter:Filter = new Filter();
+      filter["type"] = type;
+      filter["value"] = value;
+      filter["column"] = column;
+
+      return(filter);
+   }
+
+   public static LessThan(column:string, value:any, equals?:boolean) : Filter
+   {
+      let type:string = "<";
+      if (equals) type += "=";
+
+      let filter:Filter = new Filter();
+      filter["type"] = type;
+      filter["value"] = value;
+      filter["column"] = column;
+
+      return(filter);
+   }
+
+   public static Between(column:string, fr:any, to:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "between";
+      filter["values"] = [fr,to];
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static NotBetween(column:string, fr:any, to:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "between";
+      filter["values"] = [fr,to];
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static In(column:string, values:any[]) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "in";
+      filter["values"] = values;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static NotIn(column:string, values:any[]) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "not in";
+      filter["values"] = values;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static Exists(column:string, values:any[]) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "exists";
+      filter["values"] = values;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static NotExists(column:string, values:any[]) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "not exists";
+      filter["values"] = values;
+      filter["column"] = column;
+      return(filter);
+   }
+
+   public static Custom(column:string, payload:any) : Filter
+   {
+      let filter:Filter = new Filter();
+      filter["type"] = "custom";
+      filter["payload"] = payload;
       filter["column"] = column;
       return(filter);
    }
@@ -35,10 +151,33 @@ export class Filters
 export class Filter
 {
    private type:string;
+   private payload:any;
 
    private column:string;
    private columns:string[];
 
    private value:any;
    private values:any[];
+
+   public parse() : any
+   {
+      let parsed:any = {type: this.type};
+
+      if (this.column) parsed.column = this.column;
+      if (this.columns) parsed.columns = this.columns;
+
+      if (this.value) parsed.value = this.value;
+      if (this.values) parsed.values = this.values;
+
+      if (this.type == "custom")
+      {
+         if (this.payload)
+         {
+            Object.keys(parsed).forEach((attr) =>
+            {parsed[attr] = this.payload[attr];})
+         }
+      }
+
+      return(parsed);
+   }
 }
